8e7bb54cd4f44fcd498816a6187280a77d2d8201
Ahmed Alhalfa <ah.alhalfa@gmail.com>
2024-11-26 20:40:30 +0200

    Time: 123 ms (5.07%), Space: 59.8 MB (5.10%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -0,0 +1,27 @@
+function romanToInt(s: string): number {
+    const romanSympols = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
+        "IV": 4,
+        "IX": 9,
+        "XL": 40,
+        "XC": 90,
+        "CD": 400,
+        "CM": 900
+    }
+    let res = 0;
+    let sArr = s.split("");
+    for (let index = 0; index < sArr.length;)
+        {
+            console.log(res);
+            res += romanSympols[sArr[index]+sArr[index+1]] || romanSympols[sArr[index]] || 0;
+            if (romanSympols[sArr[index]+sArr[index+1]]) index += 2;
+            else index++;
+        }
+    return res;
+};
