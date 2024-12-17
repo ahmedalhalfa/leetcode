5251c53af8d93d93ee54333693aba3af56babdbb
Ahmed Alhalfa <ah.alhalfa@gmail.com>
2024-12-17 20:54:39 +0200

    Time: 64 ms (5.00%), Space: 57.9 MB (7.55%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.ts
@@ -0,0 +1,12 @@
+function scoreOfString(s: string): number {
+    return s.split("").reduce((acc, character, index) => {
+        console.log(acc);
+        if (index === s.length - 1) return acc;
+        return (Math.abs(getAscii(character) - getAscii(s[index+1]))) + acc
+    }, 0);
+};
+
+function getAscii(char): number {
+    if (!char) return 0;
+  return char.charCodeAt(0);
+}
